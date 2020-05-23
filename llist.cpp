bc777ed2bce59e56081ae1cffd5f9434bd16468f
Дарья Лапутина <laputina975@gmail.com>
2020-05-23 14:02:15 +0300

    added list

--- llist.cpp
+++ llist.cpp
@@ -1,6 +1,185 @@
-#include <llist.h>
+#include "llist.h"
+#include <stdexcept>
+
+LList::Node::Node(const int value, Node* next)
+{
+	this->value = value;
+	this->next = next;
+}
+
+LList::Node::~Node()
+{
+
+}
+
+void LList::Node::insertNext(const int& value)
+{
+	Node* newNode = new Node(value, this->next);
+	this->next = newNode;
+}
 
 
-// HERE WILL BE YOUR CODE
+void LList::Node::removeNext()
+{
+	Node* removeNode = this->next;
+	Node* newNext = removeNode->next;
+	if (newNext == nullptr) throw std::out_of_range("nonexistent node");
+	delete removeNode;
+	this->next = newNext;
+}
+
+LList::LList()
+	: _head(nullptr), _size(0)
+{
+}
+
+LList::~LList()
+{
+	delition(_head);
+}
+
+LList::Node* LList::getNode(const size_t pos) const
+{
+	if (pos < 0) {
+		throw std::out_of_range("Incorrect index");
+	}
+	else if (pos >= this->_size) {
+		throw std::out_of_range("Incorrect index");
+	}
+
+	Node* bufNode = this->_head;
+	for (int i = 0; i < pos; ++i) {
+		bufNode = bufNode->next;
+	}
+
+	return bufNode;
+}
+
+void LList::push_back(int val)
+{
+	if (_size == 0) {
+		push_front(val);
+		return;
+	}
+	insert_at(_size, val);
+}
+
+void LList::push_front(int val)
+{
+	_head = new Node(val, _head);
+	++_size;
+}
+
+void LList::pop_front()
+{
+	if (_size == 1)
+	{
+		delete _head;
+		_head = nullptr;
+		_size = 0;
+		return;
+	}
+	if (_head == nullptr)
+	{
+		throw std::out_of_range("Incorrect deletion");
+	}
+
+	Node* Buf = _head->next;
+	delete _head;
+	_head = Buf;
+	_size--;
+}
+
+size_t LList::size() const
+{
+	return _size;
+}
+
+int& LList::operator[](size_t idx)
+{
+	return getNode(idx)->value;
+}
+
+int LList::operator[](size_t idx) const
+{
+	int a =  getNode(idx)->value;
+	return a;
+
+}
+
+void LList::erase_at(size_t idx)
+{
+	if (idx > _size&& idx < 0) throw std::out_of_range("Incorrect index");
+	if (idx == 0) pop_front();
+	else
+	{
+		Node* tmp = _head;
+		for (size_t i = 0; i < idx - 1; i++)
+		{
+			tmp = tmp->next;
+		}
+		tmp->removeNext();
+		_size--;
+	}
+}
+
+void LList::insert_at(size_t idx, int val)
+{
+	if (idx < 0) {
+		throw std::out_of_range("incorrect index");
+	}
+	else if (idx > this->_size) {
+		throw std::out_of_range("incorrect index");
+	}
+
+	if (idx == 0) {
+		push_front(val);
+	}
+	else {
+		Node* bufNode = this->_head;
+		for (size_t i = 0; i < idx - 1; ++i) {
+			bufNode = bufNode->next;
+		}
+		bufNode->insertNext(val);
+		++_size;
+	}
+}
+
+void LList::reverse()
+{
+	if (_head == nullptr) return;
+	Node* curr, * next, * prev = nullptr;
+	curr = _head;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	_head = prev;
+}
+
+void LList::pop_back()
+{
+	if (_size > 1)
+	{
+		getNode(_size - 2)->removeNext();
+		_size--;
+	}
+	else pop_front();
+}
 
 
+
+
+void LList::delition(Node* node)
+{
+	if (node == nullptr) {
+		return;
+	}
+
+	Node* nextDeleteNode = node->next;
+	delete node;
+	delition(nextDeleteNode);
+}
--- llist.h
+++ llist.h
@@ -8,22 +8,38 @@
 class LList
 {
 	// HERE WILL BE YOUR CODE
+	struct Node {
+		Node(const int value, Node* next = nullptr);
+		~Node();
+
+		void insertNext(const int& value);
+		void removeNext();
+
+		int value;
+		Node* next;
+	};
 
 	public:
 
 	LList();                         // construct new collection
 	~LList();                        // free resources
 
+
+	LList::Node* getNode(const size_t pos) const;
 	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
 	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
-	void pop_back(int val);          // remove at the end          [1 2 3 4] -> [1 2 3]
-	void pop_front(int val);         // remove at the front        [1 2 3 4] -> [2 3 4]
+	void pop_back();          // remove at the end          [1 2 3 4] -> [1 2 3]
+	void pop_front();         // remove at the front        [1 2 3 4] -> [2 3 4]
 	size_t size() const;             // get actual number of items [1 2 3 4] -> 4
 	int& operator[](size_t idx);     // get rw access ot specific item addressing by idx
 	int  operator[](size_t idx) const; //get read-only access
 	void erase_at(size_t idx);       // remove item at specific position: [1 2 3 4], 2 -> [1 2 4]
 	void insert_at(size_t idx, int val); // insert item at specific position: [1 2 3 4], 1, 5 -> [1 5 2 3 4]
 	void reverse();                  // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
+private:
+	Node* _head;
+	int _size;
+	void delition(Node* node);
 };
 
 #endif //LLIST_H
